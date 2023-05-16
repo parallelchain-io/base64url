0bc398f8a62bc5c8faf1fc3851433413ca354938
Tracy Liu <tracy.liu@parallelchain.io>
2023-05-16 15:39:16 +0800

    update documentation.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Base64 URL (base64url)
 
 `Base64URL` is a modification of the main Base64 standard as described in RFC4648, the purpose of which is the ability to use the encoding result as filename or URL address.
-This library provide simple encode and decode function without additional config.
+This crate provides simple encode and decode function without additional config setup.
+
+This library is based on [base64](https://github.com/marshallpierce/rust-base64) implementation.
--- src/lib.rs
+++ src/lib.rs
@@ -3,23 +3,23 @@
     Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
 */
 
-//! Implementation of the encode and decode operations (as defined in IETF RFC 4648) on a type [Base64URL].
-//! Base64URL are Strings restricted to containing the 2^6 UTF-8 code points without padding.
+//! Implementation of the encode and decode operations on base64url (as defined in IETF RFC 4648) String.
+//! The encoded string restricted to containing the 2^6 UTF-8 code points without padding.
 //! It is the *only* binary-to-text encoding scheme used in ParallelChain F. 
 //! 
 //! In general, we use `-` and `_` instead of `+` and `/`, without paddings.
 
 use base64::{Engine as _, engine::{self, general_purpose}, alphabet};
-const CUSTOM_ENGINE: engine::GeneralPurpose = engine::GeneralPurpose::new(&alphabet::URL_SAFE, general_purpose::NO_PAD);
+const ENGINE: engine::GeneralPurpose = engine::GeneralPurpose::new(&alphabet::URL_SAFE, general_purpose::NO_PAD);
 
-/// encode takes in a slice of bytes and returns the bytes encoded as a Base64URL String. 
+/// encode takes in a slice of bytes and returns the bytes encoded as a base64url String. 
 pub fn encode<T: AsRef<[u8]>>(bytes: T) -> String { 
-    CUSTOM_ENGINE.encode(bytes)
+    ENGINE.encode(bytes)
 }
 
 /// decode takes in a string and tries to decode it into a Vector of bytes. It returns a base64::DecodeError if `string`
 /// is not valid Base64URL.
 pub fn decode<T: ?Sized + AsRef<[u8]>>(base64_url: &T) -> Result<Vec<u8>, base64::DecodeError> {
-    CUSTOM_ENGINE.decode(base64_url)
+    ENGINE.decode(base64_url)
 } 
 
